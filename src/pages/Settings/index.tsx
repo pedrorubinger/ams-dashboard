a8bf77003862c9f95dfa36e0f566df40264c33bd
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-25 17:21:56 -0300

    feat: apply SettingsForm on Settings page and add a proper text

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,78 +1,28 @@
-import { Text } from "@chakra-ui/react"
+import { Box, Text } from "@chakra-ui/react"
 
+import { SettingsForm } from "~/pages/Settings/components"
 import { PageTitle } from "~/components"
 
 export const Settings: React.FC = () => {
 	return (
-		<div>
-			<PageTitle>Settings Page</PageTitle>
+		<Box>
+			<PageTitle>Configurações da conta</PageTitle>
 
 			<Text>
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, impedit,
-				molestias facere necessitatibus placeat eos exercitationem saepe ex non,
-				earum quasi! Obcaecati soluta incidunt consequatur fugit nesciunt
-				voluptatibus quia corrupti!
+				Você pode alterar algumas informações da sua conta. Os campos marcados
+				com&nbsp;
+				<Text fontWeight="bold" color="red" as="span">
+					*
+				</Text>
+				&nbsp;são de preenchimento obrigatório.
 			</Text>
 
 			<Text>
-				Artificial intelligence has been a buzzword for several years now, and
-				for good reason. AI has the potential to revolutionize virtually every
-				industry, from healthcare to finance to transportation. But with great
-				power comes great responsibility, and there are concerns about the
-				potential negative impact of AI on society. In this article, I&apos;ll
-				explore some of the key ways that AI is changing our world and the
-				potential implications for individuals, organizations, and society as a
-				whole. First, let&apos;s define what we mean by AI. At its core, AI
-				refers to the ability of machines to perform tasks that typically
-				require human intelligence, such as learning, problem-solving, and
-				decision-making. AI can be broken down into two broad categories: narrow
-				or weak AI, which is designed to perform a specific task or set of
-				tasks, and general or strong AI, which is capable of performing any
-				intellectual task that a human can. While we are still far from
-				achieving strong AI, narrow AI is already having a significant impact on
-				our lives. One of the most visible ways that AI is changing our world is
-				through automation. AI-powered robots and software are increasingly
-				being used to perform tasks that were previously done by humans, such as
-				manufacturing, customer service, and even driving. While automation can
-				lead to increased efficiency and cost savings, it also has the potential
-				to displace workers and exacerbate existing inequalities. There are
-				concerns that automation will disproportionately affect low-skilled
-				workers, who may struggle to find new employment in a rapidly changing
-				job market. Another way that AI is changing our world is through data
-				analysis. AI algorithms are capable of processing vast amounts of data
-				and identifying patterns and insights that humans may not be able to
-				detect. This has led to significant advances in fields such as
-				healthcare, where AI is being used to diagnose diseases and develop new
-				treatments. However, there are also concerns about the potential misuse
-				of data, particularly when it comes to issues of privacy and
-				surveillance. As AI becomes more integrated into our lives, it&apos;s
-				important that we have strong regulations in place to protect
-				individuals&apos; rights and prevent abuses of power. AI also has the
-				potential to revolutionize the way we interact with technology. Natural
-				language processing (NLP) and computer vision technologies are making it
-				possible for humans to communicate with machines in more natural ways,
-				such as through voice commands and facial recognition. This is leading
-				to the development of new products and services, such as virtual
-				assistants and smart homes. However, there are also concerns about the
-				potential for these technologies to be used for malicious purposes, such
-				as deepfake videos and voice impersonation. Finally, AI is also changing
-				the way we approach problems and make decisions. AI algorithms are
-				capable of identifying patterns and predicting outcomes in ways that
-				humans may not be able to, which has led to the development of new tools
-				and strategies for problem-solving. However, there are also concerns
-				about the potential for bias and discrimination in AI decision-making,
-				particularly when it comes to issues of race, gender, and socioeconomic
-				status. It&apos;s important that we develop AI algorithms that are
-				transparent and accountable, and that we actively work to address issues
-				of bias and discrimination in our data and decision-making processes. In
-				conclusion, the impact of AI on society is both significant and complex.
-				While AI has the potential to revolutionize virtually every industry and
-				improve our lives in countless ways, there are also concerns about the
-				potential negative consequences of this technology. It&apos;s important
-				that we approach AI with a critical eye, and that we actively work to
-				mitigate the risks and ensure that this technology is used for the
-				benefit of all.
+				Por questões de segurança, você deve informar a sua senha atual para
+				atualizar qualquer informação.
 			</Text>
-		</div>
+
+			<SettingsForm />
+		</Box>
 	)
 }
