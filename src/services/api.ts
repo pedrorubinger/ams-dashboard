65049f842fca9a9c768a30c36900af298331c2c8
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-24 23:07:10 -0300

    feat: add API configuration file

---
+++ src/services/api.ts
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/named
+import Axios, { AxiosError, AxiosHeaders, AxiosRequestConfig } from "axios"
+import { getToken } from "~/utils"
+
+export const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL
+export const Api = Axios.create({ baseURL: API_BASE_URL })
+
+Api.interceptors.request.use((config: AxiosRequestConfig) => {
+	const headers: unknown = {
+		...config.headers,
+		Authorization: `Bearer ${getToken() ?? ""}`,
+		"Content-Type": "application/json",
+	}
+	return { ...config, headers: headers as AxiosHeaders }
+})
+
+Api.interceptors.response.use(
+	(response) => response,
+	async (error: AxiosError) => {
+		if (error?.response) return Promise.reject(error.response)
+
+		return Promise.reject(error)
+	}
+)
