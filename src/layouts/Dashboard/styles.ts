baf61e3fc756d38207a4fc1a5811b9b0004b35a0
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-25 17:21:07 -0300

    style: increase vertical pixel and change order of properties

--- src/layouts/Dashboard/styles.ts
+++ src/layouts/Dashboard/styles.ts
@@ -11,37 +11,38 @@ export const DashboardFlexContainer = styled(Flex).attrs(() => ({
 
 export const DashboardSideMenuContainer = styled(Flex).attrs(() => ({
 	pos: "fixed",
-	h: CONTENT_HEIGHT,
-	px: 5,
-	py: 5,
-	borderRadius: 8,
 	boxShadow: "0 4px 12px 0 rgba(0, 0, 0, 0.05)",
 	bg: "whiteAlpha.800",
 	flexDir: "column",
 	overflow: "auto",
 	width: "fit-content",
+	h: CONTENT_HEIGHT,
+	px: 5,
+	py: 5,
+	borderRadius: 8,
 }))``
 
 export const DashboardMenuListContainer = styled(Flex).attrs(() => ({
 	w: "100%",
+	flexDir: "column",
 	m: 0,
 	p: 0,
-	flexDir: "column",
 	gap: 2,
 }))``
 
 export const DashboardContentContainer = styled(Flex).attrs(() => ({
 	flexDirection: "column",
+	w: "100%",
 	minHeight: CONTENT_HEIGHT,
 }))``
 
 export const DashboardOutletContainer = styled(Flex).attrs(() => ({
 	h: "fit-content",
-	px: 10,
-	py: 5,
-	borderRadius: 8,
 	boxShadow: "0 4px 12px 0 rgba(0, 0, 0, 0.05)",
 	bg: "whiteAlpha.800",
 	overflow: "auto",
 	w: "100%",
+	px: 10,
+	py: 7,
+	borderRadius: 8,
 }))``
