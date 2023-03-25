b077b5d585d37ab84fde426206f5a1f58012257f
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-25 14:26:21 -0300

    refactor: prevent user to expand menu when window width is small

--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -5,25 +5,38 @@ import { List as MenuIcon } from "phosphor-react"
 
 import { MenuStatus } from "~/interfaces"
 import { sideMenuItems } from "~/router/routes"
-import { SideMenuItem } from "~/layouts/Dashboard/components"
+import { useWindowWidth } from "~/hooks"
 import {
 	DashboardFlexContainer,
 	DashboardMenuListContainer,
 	DashboardOutletContainer,
 	DashboardSideMenuContainer,
 } from "~/layouts/Dashboard/styles"
+import { SideMenuItem } from "~/layouts/Dashboard/components"
 
 const CONTENT_HORIZONTAL_GAP = 40
+const EXPANDED_MENU_MAX_WIDTH = 640
 
 export function DashboardLayout() {
 	const { pathname } = useLocation()
 	const sideMenuRef = useRef<HTMLDivElement>(null)
+	const { width } = useWindowWidth()
 	const [menuStatus, setMenuStatus] = useState<MenuStatus>("expanded")
 	const [sideMenuWidth, setSideMenuWidth] = useState<number>()
+
+	const isWindowWidthSmall: boolean = width <= EXPANDED_MENU_MAX_WIDTH
+
+	/** TO DO: Transform side menu into a top menu when screen is small. */
 
 	useEffect(() => {
 		setSideMenuWidth(sideMenuRef?.current?.offsetWidth)
 	}, [menuStatus])
+
+	useEffect(() => {
+		if (isWindowWidthSmall) {
+			setMenuStatus("collapsed")
+		}
+	}, [isWindowWidthSmall])
 
 	const toggleMenuStatus = () => {
 		setMenuStatus((prev) => (prev === "expanded" ? "collapsed" : "expanded"))
@@ -32,15 +45,17 @@ export function DashboardLayout() {
 	return (
 		<DashboardFlexContainer>
 			<DashboardSideMenuContainer ref={sideMenuRef}>
-				<Flex mb={5}>
-					<IconButton
-						background="none"
-						aria-label="menu"
-						_hover={{ background: "none" }}
-						icon={<MenuIcon size={18} cursor="pointer" />}
-						onClick={toggleMenuStatus}
-					/>
-				</Flex>
+				{!isWindowWidthSmall && (
+					<Flex mb={5}>
+						<IconButton
+							background="none"
+							aria-label="menu"
+							_hover={{ background: "none" }}
+							icon={<MenuIcon size={18} cursor="pointer" />}
+							onClick={toggleMenuStatus}
+						/>
+					</Flex>
+				)}
 
 				{/* ITEMS */}
 				<DashboardMenuListContainer>
