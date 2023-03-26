bd7e0d5d595ea069d7ec1bd88724d5bf93d19b73
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-26 14:58:34 -0300

    chore: add newPassword validations on change account data (Settings)

--- src/pages/Settings/components/Form/schema.ts
+++ src/pages/Settings/components/Form/schema.ts
@@ -7,7 +7,14 @@ export const AccountSchema = Yup.object<FormValues>().shape({
 	password: Yup.string().required("Insira sua senha atual!"),
 	newPassword: Yup.string().when("changePassword", {
 		is: true,
-		then: () => Yup.string().required("Insira sua nova senha!"),
+		then: () =>
+			Yup.string()
+				.min(6, "A nova senha deve ter pelo menos 6 caracteres!")
+				.required("Insira sua nova senha!")
+				.notOneOf(
+					[Yup.ref("password"), null],
+					"As senhas devem ser diferentes!"
+				),
 		otherwise: () => Yup.string(),
 	}),
 })
