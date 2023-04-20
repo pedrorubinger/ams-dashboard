63646f1c931d30d84da19b0206b77b952873abba
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-04-20 20:50:29 -0300

    refactor: adjust data structure when calculating values by month and year

--- src/pages/Partners/utils/helpers.ts
+++ src/pages/Partners/utils/helpers.ts
@@ -12,18 +12,30 @@ interface GetGroupedValuesResponse {
 export const getGroupedValues = (
 	records: PartnerFinancialSupport[]
 ): GetGroupedValuesResponse => {
-	const annualySum: number = records.reduce(
+	const currentYear = new Date().getFullYear()
+	const groupedByMonth: PartnerFinancialSupport[][] = Array.from(
+		new Set(records.map((record) => record.billingMonth))
+	)
+		.map((month) =>
+			records.filter(
+				(record) =>
+					record.billingMonth === month && record.billingYear === currentYear
+			)
+		)
+		.filter((record) => record.length)
+
+	const monthlySum: PartnerFinancialSupportPerMonth[] = groupedByMonth.map(
+		(record) => ({
+			month: record[0].billingMonth,
+			year: record[0].billingYear,
+			value: record.reduce((prev, curr) => prev + curr.value, 0),
+		})
+	)
+
+	const annualySum: number = monthlySum.reduce(
 		(prev, curr) => prev + curr.value,
 		0
 	)
-	const groupedByMonth = Array.from(
-		new Set(records.map((record) => record.billingMonth))
-	).map((month) => records.filter((record) => record.billingMonth === month))
-	const monthlySum = groupedByMonth.map((record) => ({
-		month: record[0].billingMonth,
-		year: record[0].billingYear,
-		value: record.reduce((prev, curr) => prev + curr.value, 0),
-	}))
 
 	return { annualySum, monthlySum }
 }
