a425fcad1a82bd37785b2e541f5e5c659da6304f
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-04-21 11:20:54 -0300

    feat: add Tooltip component to each report card title

--- src/pages/PartnerDonations/index.tsx
+++ src/pages/PartnerDonations/index.tsx
@@ -1,23 +1,33 @@
 import React from "react"
-import { Link, Text } from "@chakra-ui/react"
+import { Flex, Link, Text } from "@chakra-ui/react"
 import { Link as RouterLink } from "react-router-dom"
 
+import {
+	PartnerDonationBillingMonth as Month,
+	PartnerDonationBillingMonthLabel as MonthLabel,
+} from "~/interfaces"
+import { dateFormatter } from "~/utils"
 import { ReportsContainer } from "~/pages/PartnerDonations/styles"
 import { ContentSection, PageTitle } from "~/components"
+import { Tooltip } from "~/components/Tooltip"
 
 interface Props {}
 
 export const PartnerDonations: React.FC<Props> = () => {
+	const date = new Date()
+	const today = dateFormatter.format(date)
+	const month: string =
+		MonthLabel[Month[date.getMonth() + 1] as keyof typeof MonthLabel]
+	const year = String(date.getFullYear())
+
 	return (
 		<>
 			<ContentSection>
 				<PageTitle>Contribuições</PageTitle>
 
 				<Text>
-					Nesta página você encontra todas as contribuições registradas.
-					<br />
-					Se você deseja ver as contribuições de cada associado, acesse a
-					página&nbsp;
+					Nesta página você encontra todas as contribuições registradas. Se você
+					deseja ver as contribuições de cada associado, acesse a página&nbsp;
 					<Link to="/associados" color="primary.400" as={RouterLink}>
 						Associados
 					</Link>
@@ -27,27 +37,50 @@ export const PartnerDonations: React.FC<Props> = () => {
 
 			<ReportsContainer>
 				<ContentSection mt={6}>
-					<Text color="gray.500" fontSize={18}>
-						Diário
-					</Text>
+					<Flex alignItems="center">
+						<Text color="gray.500" fontSize={18}>
+							Diário
+						</Text>
+						&nbsp;
+						<Tooltip
+							label={`Somatória de todos os lançamentos feitos no dia de hoje (${today})`}
+							placement="top-start"
+						/>
+					</Flex>
+
 					<Text fontWeight="bold" fontSize={22} color="blackAlpha.700">
 						R$ 144,55
 					</Text>
 				</ContentSection>
 
 				<ContentSection mt={6}>
-					<Text color="gray.500" fontSize={18}>
-						Mensal
-					</Text>
+					<Flex alignItems="center">
+						<Text color="gray.500" fontSize={18}>
+							Mensal
+						</Text>
+						&nbsp;
+						<Tooltip
+							label={`Somatória de todos os lançamentos feitos no mês corrente (${month})`}
+							placement="top-start"
+						/>
+					</Flex>
+
 					<Text fontWeight="bold" fontSize={22} color="blackAlpha.700">
 						R$ 484,62
 					</Text>
 				</ContentSection>
 
 				<ContentSection mt={6}>
-					<Text color="gray.500" fontSize={18}>
-						Anual
-					</Text>
+					<Flex alignItems="center">
+						<Text color="gray.500" fontSize={18}>
+							Anual
+						</Text>
+						&nbsp;
+						<Tooltip
+							label={`Somatória de todos os lançamentos feitos no ano corrente (${year})`}
+							placement="top-start"
+						/>
+					</Flex>
 					<Text fontWeight="bold" fontSize={22} color="blackAlpha.700">
 						R$ 9.144,55
 					</Text>
