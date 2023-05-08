a1f6ab4509ea9cf48152a4a5680573bd8830e654
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-05-08 17:07:32 -0300

    style: adjust font size

--- src/pages/PartnerDonations/components/Reports/index.tsx
+++ src/pages/PartnerDonations/components/Reports/index.tsx
@@ -28,8 +28,8 @@ export const PartnerDonationsReport: React.FC<Props> = ({
 		<>
 			<ContentSection mt={6}>
 				<Flex alignItems="center">
-					<Text color="gray.500" fontSize={18}>
-						Diário
+					<Text color="gray.500" fontSize={15}>
+						Diário ({today})
 					</Text>
 					&nbsp;
 					<Tooltip
@@ -38,15 +38,15 @@ export const PartnerDonationsReport: React.FC<Props> = ({
 					/>
 				</Flex>
 
-				<Text fontWeight="bold" fontSize={22} color="blackAlpha.700">
+				<Text fontWeight="bold" fontSize={18} color="blackAlpha.700">
 					R$ 144,55
 				</Text>
 			</ContentSection>
 
 			<ContentSection mt={6}>
 				<Flex alignItems="center">
-					<Text color="gray.500" fontSize={18}>
-						Mensal
+					<Text color="gray.500" fontSize={15}>
+						Mensal ({month})
 					</Text>
 					&nbsp;
 					<Tooltip
@@ -55,15 +55,15 @@ export const PartnerDonationsReport: React.FC<Props> = ({
 					/>
 				</Flex>
 
-				<Text fontWeight="bold" fontSize={22} color="blackAlpha.700">
+				<Text fontWeight="bold" fontSize={18} color="blackAlpha.700">
 					R$ 484,62
 				</Text>
 			</ContentSection>
 
 			<ContentSection mt={6}>
 				<Flex alignItems="center">
-					<Text color="gray.500" fontSize={18}>
-						Anual
+					<Text color="gray.500" fontSize={15}>
+						Anual ({year})
 					</Text>
 					&nbsp;
 					<Tooltip
@@ -71,15 +71,15 @@ export const PartnerDonationsReport: React.FC<Props> = ({
 						placement="top-start"
 					/>
 				</Flex>
-				<Text fontWeight="bold" fontSize={22} color="blackAlpha.700">
+				<Text fontWeight="bold" fontSize={18} color="blackAlpha.700">
 					R$ 914,55
 				</Text>
 			</ContentSection>
 
 			{!!hasFilter && (
-				<ContentSection mt={6}>
+				<ContentSection mt={6} flexGrow={1}>
 					<Flex alignItems="center">
-						<Text color="gray.500" fontSize={18}>
+						<Text color="gray.500" fontSize={15}>
 							Período
 						</Text>
 						&nbsp;
@@ -90,7 +90,7 @@ export const PartnerDonationsReport: React.FC<Props> = ({
 							placement="top-start"
 						/>
 					</Flex>
-					<Text fontWeight="bold" fontSize={22} color="blackAlpha.700">
+					<Text fontWeight="bold" fontSize={18} color="blackAlpha.700">
 						R$ 1.431,99
 					</Text>
 				</ContentSection>
