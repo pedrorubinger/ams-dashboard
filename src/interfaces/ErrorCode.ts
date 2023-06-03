b64941d44fe617cd3f9bceb9d9aba25e4687fe5f
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-06-02 23:00:40 -0300

    chore: add 404_REGISTRATION_ID_NOT_FOUND error message

--- src/interfaces/ErrorCode.ts
+++ src/interfaces/ErrorCode.ts
@@ -12,6 +12,7 @@ enum ErrorCode {
 	/** 404 */
 	"404_USER_NOT_FOUND" = "Não encontramos este usuário.",
 	"404_TENANT_NOT_FOUND" = "Não encontramos esta instituição.",
+	"404_REGISTRATION_ID_NOT_FOUND" = "Não encontramos o último registro de matrícula do associado.",
 	/** 400 */
 	"400_EMAIL_ALREADY_REGISTERED" = "Este email já está sendo utilizado.",
 	"400_INVALID_CREDENTIALS" = "Usuário ou senha incorretos.",
