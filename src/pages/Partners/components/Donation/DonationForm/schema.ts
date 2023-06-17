10330dba7b9a794a96b9909ffd2f568882b7fc4d
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-06-17 14:00:36 -0300

    chore: add incomeDate prop to NewDonation schema

--- src/pages/Partners/components/Donation/DonationForm/schema.ts
+++ src/pages/Partners/components/Donation/DonationForm/schema.ts
@@ -5,18 +5,7 @@ export const NewDonationSchema = Yup.object().shape({
 		.of(Yup.string())
 		.required("Selecione o mês da competência!")
 		.min(1, "Selecione pelo menos um mês da competência!"),
-	// billingDate: Yup.array()
-	// 	.of(Yup.string())
-	// 	.required("Selecione a data da competência!")
-	// 	.min(1, "Selecione pelo menos uma data da competência!")
-	// 	.test(
-	// 		"is-billing-date-unique",
-	// 		"As datas da competência devem ser únicas!",
-	// 		(value: (string | undefined)[]) => {
-	// 			const uniqueValues = new Set(value.filter((v) => v !== undefined))
-	// 			return uniqueValues.size === value.length
-	// 		}
-	// 	),
+	incomeDate: Yup.string().required("Informe a data em que o valor foi pago!"),
 	category: Yup.string().required("Selecione a categoria!"),
 	value: Yup.string().required("Informe o valor do lançamento!"),
 	description: Yup.string()
