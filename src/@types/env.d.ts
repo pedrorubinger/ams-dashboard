66bfe3b1d4feb86e0b1297b8c7c71b66193ef750
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-06-17 10:38:40 -0300

    chore: add environment variable

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,9 +1,16 @@
 /// <reference types="vite/client" />
 
+enum AppEnvironment {
+	LOCAL = "LOCAL",
+	STAGING = "STAGING",
+	PRODUCTION = "PRODUCTION",
+}
+
 interface ImportMetaEnv {
 	readonly VITE_API_BASE_URL: string
 	readonly VITE_CREATOR_GITHUB_URL: string
 	readonly VITE_APPLICATION_VERSION: string
+	readonly VITE_APPLICATION_ENVIRONMENT: AppEnvironment
 }
 
 interface ImportMeta {
