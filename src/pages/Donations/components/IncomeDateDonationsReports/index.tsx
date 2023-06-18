a3dec1231702730cc1d660cced4c25097cd4ac35
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-06-17 21:27:37 -0300

    chore: add helpers file

--- src/pages/Donations/components/IncomeDateDonationsReports/index.tsx
+++ src/pages/Donations/components/IncomeDateDonationsReports/index.tsx
@@ -6,8 +6,9 @@ import {
 	useDonationCurrentDates,
 	useDonationReports,
 } from "~/pages/Donations/hooks"
+import { priceFormatter } from "~/utils"
+import { formatPlainDateToBr } from "~/pages/Donations/utils"
 import { ReportsSection } from "~/pages/Donations/components/Reports"
-import { dateFormatter, priceFormatter, transformDateToISO } from "~/utils"
 
 interface Props {
 	hasFilter: boolean
@@ -25,14 +26,11 @@ export const IncomeDateDonationsReports: React.FC<Props> = ({
 		mode: "INCOME",
 	})
 
-	const formatDate = (value: string) =>
-		dateFormatter.format(new Date(transformDateToISO(value)))
-
 	const formattedRange = dateRange
-		? `${formatDate(dateRange[0])} - ${formatDate(dateRange[1])}`
+		? `${formatPlainDateToBr(dateRange[0])} - ${formatPlainDateToBr(
+				dateRange[1]
+		  )}`
 		: ""
-
-	console.log("date range", dateRange)
 
 	return (
 		<Flex flexDirection="column" width="100%">
