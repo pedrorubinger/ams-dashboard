a7c4b1213286e934ac3e2f5b8caa791370dd9e60
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-07-02 13:35:45 -0300

    chore: add userId to donation interface

--- src/interfaces/Donation.ts
+++ src/interfaces/Donation.ts
@@ -80,6 +80,7 @@ export interface NewDonationDrawerProps {
 export interface Donation {
 	id: string
 	partnerId: string
+	userId: string
 	billingDate: string[]
 	category: DonationCategory
 	incomeDate: string
