7c465aacadcace10660228b75439046077fa4101
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-07-02 15:01:18 -0300

    chore: add "STAGING" env variable

--- src/utils/constants/env.ts
+++ src/utils/constants/env.ts
@@ -1,4 +1,4 @@
 const APPLICATION_ENVIRONMENT: string = import.meta.env
 	.VITE_APPLICATION_ENVIRONMENT
 
-export const isDevEnv = ["LOCAL"].includes(APPLICATION_ENVIRONMENT)
+export const isDevEnv = ["LOCAL", "STAGING"].includes(APPLICATION_ENVIRONMENT)
